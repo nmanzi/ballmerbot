22ee068f803eaacbe0dbb5cdc179ca1700dbf3fd
Nathaniel Manzi <nathan@nmanzi.com>
2014-04-04 18:02:21 +0800

    title grabber: ignore vimeo links

--- plugins/urltitlegrabber.py
+++ plugins/urltitlegrabber.py
@@ -1,4 +1,4 @@
-# This plugin grabs the <title> tag from a mentioned URL 
+# This plugin grabs the <title> tag from a mentioned URL
 # and feeds it back to the channel
 
 from util import hook, http, urlnorm
@@ -13,5 +13,6 @@ def urltitlegrabber(match):
   url = urlnorm.normalize(match.group().encode('utf-8'))
   t = http.get_html(url)
   title = t.find(".//title").text.strip().replace('\t', " ").replace('\n', "")
-  if title and not re.search("twitter.com", url) and not re.search("youtube.com", url):
+  # I really need to put this in a lookup
+  if title and not re.search("twitter.com", url) and not re.search("vimeo.com", url) and not re.search("youtube.com", url):
     return "[%s]" % title
